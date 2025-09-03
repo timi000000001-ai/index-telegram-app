00a6eafe4c6a192fd98da9a8d3e95f5236f3661e
timi000000001-ai <timi000000001@gmail.com>
2025-09-03 21:21:28 +0800

    fix: configure svelte.config.js for github pages

--- index-telegram-app/svelte.config.js
+++ index-telegram-app/svelte.config.js
@@ -1,20 +1,17 @@
+import { vitePreprocess } from '@sveltejs/kit/vite';
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
-
 	kit: {
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: null,
-			precompress: false,
-			strict: true
+			strict: false
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/index-telegram-app' : ''
+			base: dev ? '' : '/index-telegram-app'
 		}
 	}
 };
